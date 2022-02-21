76878bcaf52645149c3aed17725e78805b59c1ef
sangbong_dmk <sangbong@datamarketing.co.kr>
2022-02-21 15:40:03 +0900

    tailwind css test

--- 854-fe-playground/src/App.tsx
+++ 854-fe-playground/src/App.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
+import wing_chun from './wing_chun.png'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='container mx-auto'> 
+          <h1 className='p-4 text-4xl font-bold text-center'>
+            ★프론트엔드 수련장★
+          </h1>
+          <div className='flex justify-center'>
+            <img className='w-full px-[24%]' src={wing_chun} alt="영춘권" />
+          </div>
+          <div className="w-full text-2xl text-center text-blue-700">
+          <a
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            리액트 배우기
+          </a>
+          </div>
+        </div>
       </header>
     </div>
   );
