7246588611bef2e34d08881ea53f5215043c7f80
sangbong_dmk <sangbong@datamarketing.co.kr>
2022-02-21 18:28:52 +0900

    add : 드래그연습 추가

--- 854-fe-playground/src/App.tsx
+++ 854-fe-playground/src/App.tsx
@@ -1,28 +1,38 @@
 import React from 'react';
-import wing_chun from './wing_chun.png'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import DragPage from 'DragPage';
+import LandingPage from 'LandingPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+        <header className='p-4 text-4xl font-bold text-center'>
+          ★프론트엔드 수련장★
+        </header>
+
+        
         <div className='container mx-auto'> 
+          <BrowserRouter>
           <h1 className='p-4 text-4xl font-bold text-center'>
-            ★프론트엔드 수련장★
+            react-router-dom v6다 쉬먀
           </h1>
-          <div className='flex justify-center'>
-            <img className='w-full px-[24%]' src={wing_chun} alt="영춘권" />
+          <div className='flex flex-row p-3 justify-evenly'>
+            <Link to="/"><div className='p-6 rounded-md outline outline-offset-2 outline-4 outline-blue-500 active:bg-slate-400'>랜딩페이지</div></Link>
+            <Link to="/drag"><div className='p-6 rounded-md outline outline-offset-2 outline-4 outline-blue-500 active:bg-slate-400'>드래그테스트</div></Link>
           </div>
-          <div className="w-full text-2xl text-center text-blue-700">
-          <a
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            워씽 리액트 배워라 쉬먀
-          </a>
-          </div>
+
+            <Routes>
+              <Route path="/" element={<LandingPage />} />
+              <Route path="/drag" element={<DragPage />} />
+            </Routes>
+          </BrowserRouter>
         </div>
-      </header>
+
     </div>
   );
 }
